0716d1dd1efe4593d682fea05c2f6ebc39a9ae1d
jeongho <hyeon9782@gmail.com>
2024-06-15 18:53:28 +0900

    feat: TreeItem 컴포넌트 기능 구현

--- src/components/shared/tree-item.tsx
+++ src/components/shared/tree-item.tsx
@@ -1,5 +1,38 @@
-const TreeItem = () => {
-  return <div>TreeItem</div>;
+import { MouseEvent, useState } from "react";
+
+type TreeItem = {
+  label: string;
+  children: TreeItem[];
+};
+
+type Props = {
+  items: TreeItem[];
+};
+const TreeItem = ({ items }: Props) => {
+  const [openIndexes, setOpenIndexes] = useState<number[]>([]);
+
+  const toggleItem =
+    (index: number, item: TreeItem) => (e: MouseEvent<HTMLElement>) => {
+      e.stopPropagation();
+
+      setOpenIndexes((prevOpenIndexes) =>
+        prevOpenIndexes.includes(index)
+          ? prevOpenIndexes.filter((i) => i !== index)
+          : [...prevOpenIndexes, index]
+      );
+    };
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li onClick={toggleItem(index, item)}>
+          <span>{item.label}</span>
+          {openIndexes.includes(index) && item?.children && (
+            <TreeItem items={item.children} />
+          )}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default TreeItem;

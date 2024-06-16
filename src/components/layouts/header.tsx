24628308d6c60f1484cba2f134ee62249670d13f
jeongho <hyeon9782@gmail.com>
2024-06-16 22:46:29 +0900

    feat: Header 컴포넌트 구현

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
 const Header = () => {
-  return <header>header</header>;
+  const { pathname } = useLocation();
+  const [title, setTitle] = useState("");
+
+  const createTitle = (pathname: string) => {
+    let title = "";
+    switch (pathname) {
+      case "/searched":
+        title = "Seached";
+        break;
+      case "/statistics":
+        title = "Statistics";
+        break;
+    }
+
+    setTitle(title);
+  };
+
+  useEffect(() => {
+    createTitle(pathname);
+  }, []);
+  return (
+    <header>
+      <div>{title}</div>
+      <div></div>
+    </header>
+  );
 };
 
 export default Header;
